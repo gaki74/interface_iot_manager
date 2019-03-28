268be817f3e2e83bc0de48fb8d372faea5e703fc
Hirogaki Masaki <gaki@monku.net>
2019-03-28 12:00:09 +0900

    cookie-parserを削除

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var basicAuth = require('basic-auth');
 
@@ -18,7 +17,6 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
   if (req.url.match(/^\/sensors\/\d+\/datum$/) && req.method === 'POST') {
