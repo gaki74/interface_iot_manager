d1e199d10b952c050fede0bdcbd8c108c8b53b8c
Hirogaki Masaki <gaki@monku.net>
2019-03-29 13:15:46 +0900

    データ受信時MQTT通信機能を実装

--- routes/sensors.js
+++ routes/sensors.js
@@ -10,43 +10,62 @@ const options = { useNewUrlParser: true };
 const uri = process.env.MONGODB_URI;
 const dbName = uri.split('/').pop();
 
+// Heroku Addonとして提供されているCluodMQTTを使用します。
+// 以下のコマンドでcat（無料）プランでMQTTを有効化できます。
+// `$ heroku addons:create cloudmqtt:cat`
+// また、Node.jsからMQTTを操作するためには以下のコマンドを実行し、必要な
+// パッケージをインストールする必要があります。
+// `$ npm install --save mqtt`
+var mqttClient = mqtt.connect(process.env.CLOUDMQTT_URL);
+const mqttTopic = (sensorId) => `alerts/sensor${sensorId}`;
+
+// NQTT通信機能の追加に伴い、`threshold`というキーと追加します。
+// センサーがこれよりも大きな値を計測した場合、警報を出します。
 const sensors = {
-  1: { strKey: '[上記で作成した共通鍵1]' },
+  1: { strKey: '[上記で作成した共通鍵1]', threshold: 30 },
   2: { strKey: '[上記で作成した共通鍵2]' }
 };
 
 /* POST datum */
-router.post('/:id/datum', function(request, response) {
-  var id = Number(request.params['id']);
-  var sensor = sensors[id];
+mqttClient.on('connect', () => {
+  router.post('/:id/datum', function(request, response) {
+    var id = Number(request.params['id']);
+    var sensor = sensors[id];
 
-  if (sensor) {
-    var datum = decrypt(request.body, sensor.strKey);
+    if (sensor) {
+      var datum = decrypt(request.body, sensor.strKey);
 
-    if (datum) {
-      datum.sensorId = id;
+      if (datum) {
+        datum.sensorId = id;
 
-      mongoClient.connect(uri, options, (error, client) => {
-        if (error) {
-          response.status(500).send(error);
-        } else {
-          var collection = client.db(dbName).collection('data');
-          collection.insertOne(datum, (error) => {
-            if (error) {
-              response.status(500).send(error)
-            } else {
-              response.status(201).send(datum);
-            }
-          });
-          client.close();
-        }
-      });
+        mongoClient.connect(uri, options, (error, mongo) => {
+          if (error) {
+            response.status(500).send(error);
+          } else {
+            var collection = mongo.db(dbName).collection('data');
+            collection.insertOne(datum, (error) => {
+              if (error) {
+                response.status(500).send(error)
+              } else {
+                if (sensor.threshold && datum.value >= sensor.threshold) {
+                  mqttClient.publish(
+                    mqttTopic(datum.sensorId),
+                    datum.date.toString()
+                  );
+                }
+                response.status(201).send(datum);
+              }
+            });
+            mongo.close();
+          }
+        });
+      } else {
+        response.status(400).send('undecryptable request');
+      }
     } else {
-      response.status(400).send('undecryptable request');
+      response.status(404).send('no sensor found');
     }
-  } else {
-    response.status(404).send('no sensor found');
-  }
+  });
 });
 
 /* GET data */
